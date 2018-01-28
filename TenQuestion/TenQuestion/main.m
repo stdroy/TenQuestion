76f4fe0d6cbc6cfbabb394fb34161813743aacd0
stdroy <403697496@qq.com>
2018-01-28 16:22:39 +0800

    add project

---
+++ TenQuestion/TenQuestion/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TenQuestion
+//
+//  Created by ye on 2018/1/28.
+//  Copyright © 2018年 ye. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
